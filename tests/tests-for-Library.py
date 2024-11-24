1d9d9a0fbe306cd791f0ccf004e31351457f10ef
Apizh <aptizh@gmail.com>
2024-11-25 01:43:27 +0400

    Finished tests to check the created paths

--- tests/tests-for-Library.py
+++ tests/tests-for-Library.py
@@ -7,13 +7,22 @@ class TestLibrary(unittest.TestCase):
     """Тестирование создания файла хранения библиотеки."""
 
     def test_path_creation(self):
-        """Тест: Проверка создания пути файла"""
-        file_exists = LibraryManager()
-        self.assertEqual(file_exists.storage_file, STORAGE_FILE)
-        self.assertTrue(str(file_exists.storage_file).endswith('library.json'))
+        """Тест: Проверка создания пути файла без передачи пути"""
+        file_path = LibraryManager()
+        self.assertTrue(str(file_path.storage_file).endswith('library.json'))
 
     def test_incorrect_path_creation(self):
         """Тест: Передаём некорректный путь, на выходе получаем обработку этого пути"""
-        file_exists = LibraryManager("/некорректный путь/").storage_file
-        self.assertNotEqual(file_exists, STORAGE_FILE)
-        self.assertTrue(str(file_exists).endswith('library.json'))
+        file_path = LibraryManager("/некорректный путь/").storage_file
+        self.assertNotEqual(file_path, STORAGE_FILE)
+        self.assertTrue(str(file_path).endswith('library.json'))
+
+    def test_input_path(self):
+        """Тест: указание существующей директории(абсолютный путь) на выходе
+        полный путь к файлу 'директория/library.json'"""
+        adress = r"C:\Users\admin\PycharmProjects\library-app"
+        file_path = LibraryManager(adress).storage_file
+        # Проверяем что это не файл
+        self.assertFalse(file_path.is_file())
+        # Проверка создания абсолютного пути к файлу adress + library.json
+        self.assertEqual(str(file_path), adress + '\library.json')
